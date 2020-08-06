5c36b1dafe012d9ecd06973675ea3528f2585f22
sarahbarron <sarahflavin@yahoo.com>
2020-08-06 20:46:53 +0100

    add to favorite extended - remove movie if it appears in any of the states and only add it once to favorites

--- src/contexts/moviesContext.js
+++ src/contexts/moviesContext.js
@@ -9,12 +9,52 @@ export const MoviesContext = createContext(null);
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case "add-favorite":
+      if (action.payload.trendingmovie) {
+        state.trending = state.trending.filter(
+          (m) => m.id !== action.payload.trendingmovie.id
+        );
+      }
+      if (action.payload.movie) {
+        state.movies = state.movies.filter(
+          (m) => m.id !== action.payload.movie.id
+        );
+      }
+      if (action.payload.upcomingmovie) {
+        state.upcoming = state.upcoming.filter(
+          (m) => m.id !== action.payload.upcomingmovie.id
+        );
+      }
+      if (action.payload.genremovie) {
+        state.genremovies = state.genremovies.filter(
+          (m) => m.id !== action.payload.genremovie.id
+        );
+      }
+
+      return {
+        trending: [...state.trending],
+        upcoming: [...state.upcoming],
+        movies: [...state.movies],
+        genremovies: [...state.genremovies],
+        // upcoming: [
+        //   state.upcoming.filter(
+        //     (m) => m.id !== action.payload.upcomingmovie.id
+        //   ),
+        // ],
+        // genremovies: [
+        //   state.genremovies.filter(
+        //     (m) => m.id !== action.payload.genremovie.id
+        //   ),
+        // ],
+        // movies: [state.movies.filter((m) => m.id !== action.payload.movie.id)],
+        favorites: [...state.favorites, action.payload.favmovie],
+      };
     case "add-favorite-movies":
       return {
         trending: [...state.trending],
         upcoming: [...state.upcoming],
         genremovies: [...state.genremovies],
-        movies: state.movies.filter((m) => m.id !== action.payload.movie.id),
+        movies: [state.movies.filter((m) => m.id !== action.payload.movie.id)],
         favorites: [...state.favorites, action.payload.movie],
       };
     case "add-favorite-trending":
@@ -87,33 +127,60 @@ const MoviesContextProvider = (props) => {
     genremovies: [],
   });
 
-  const addToFavorites = (movieId, movieGroup) => {
-    console.log("Movie Group: ", movieGroup);
-    if (movieGroup === "movies") {
-      const index = state.movies.map((m) => m.id).indexOf(movieId);
-      dispatch({
-        type: "add-favorite-movies",
-        payload: { movie: state.movies[index] },
-      });
-    } else if (movieGroup === "trending") {
-      const index = state.trending.map((m) => m.id).indexOf(movieId);
-      dispatch({
-        type: "add-favorite-trending",
-        payload: { movie: state.trending[index] },
-      });
-    } else if (movieGroup === "upcoming") {
-      const index = state.upcoming.map((m) => m.id).indexOf(movieId);
-      dispatch({
-        type: "add-favorite-upcoming",
-        payload: { movie: state.upcoming[index] },
-      });
-    } else if (movieGroup === "genre") {
-      const index = state.genremovies.map((m) => m.id).indexOf(movieId);
-      dispatch({
-        type: "add-favorite-genre",
-        payload: { movie: state.genremovies[index] },
-      });
+  const addToFavorites = (movieId) => {
+    const movieindex = state.movies.map((m) => m.id).indexOf(movieId);
+    const trendingindex = state.trending.map((m) => m.id).indexOf(movieId);
+    const upcomingindex = state.upcoming.map((m) => m.id).indexOf(movieId);
+    const genremoviesindex = state.genremovies
+      .map((m) => m.id)
+      .indexOf(movieId);
+    let favmovie = {};
+    if (movieindex >= 0) {
+      favmovie = state.movies[movieindex];
+    } else if (trendingindex >= 0) {
+      favmovie = state.trending[trendingindex];
+    } else if (upcomingindex >= 0) {
+      favmovie = state.upcoming[upcomingindex];
+    } else if (genremoviesindex >= 0) {
+      favmovie = state.genremovies[genremoviesindex];
     }
+
+    dispatch({
+      type: "add-favorite",
+      payload: {
+        favmovie: favmovie,
+        movie: state.movies[movieindex],
+        trendingmovie: state.trending[trendingindex],
+        upcomingmovie: state.upcoming[upcomingindex],
+        genremovie: state.genremovies[genremoviesindex],
+      },
+    });
+
+    // if (movieGroup === "movies") {
+    //   const index = state.movies.map((m) => m.id).indexOf(movieId);
+    //   dispatch({
+    //     type: "add-favorite-movies",
+    //     payload: { movie: state.movies[index] },
+    //   });
+    // } else if (movieGroup === "trending") {
+    //   const index = state.trending.map((m) => m.id).indexOf(movieId);
+    //   dispatch({
+    //     type: "add-favorite-trending",
+    //     payload: { movie: state.trending[index] },
+    //   });
+    // } else if (movieGroup === "upcoming") {
+    //   const index = state.upcoming.map((m) => m.id).indexOf(movieId);
+    //   dispatch({
+    //     type: "add-favorite-upcoming",
+    //     payload: { movie: state.upcoming[index] },
+    //   });
+    // } else if (movieGroup === "genre") {
+    //   const index = state.genremovies.map((m) => m.id).indexOf(movieId);
+    //   dispatch({
+    //     type: "add-favorite-genre",
+    //     payload: { movie: state.genremovies[index] },
+    //   });
+    // }
   };
 
   const addReview = (movie, review) => {
